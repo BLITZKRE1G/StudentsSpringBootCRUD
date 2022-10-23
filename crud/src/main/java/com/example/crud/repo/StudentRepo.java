1e310c84a0fa10a9ee0088a03f89095a92bb9e2c
blitz-23 <94664667+blitz-23@users.noreply.github.com>
2022-10-23 16:55:35 +0530

    Delete crud directory
    Need to Improve Styling. And Details after Each Operation.

--- crud/src/main/java/com/example/crud/repo/StudentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud.repo;
-
-import com.example.crud.model.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepo extends CrudRepository<Student, Integer> {
-}
